995ee529666ec219c99758c848cfd34226b38daa
meiye_ts <hello@itcast.cn>
2025-04-28 21:05:47 +0800

    03-2店铺管理开发：查询店铺列表（基本完成） 1. 修改了项目相关配置（name改为MeiYe）

--- src/views/manage/store/index.vue
+++ src/views/manage/store/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+    <!-- 顶部搜索框 -->
+    <el-form
+      :model="queryParams"
+      ref="queryRef"
+      :inline="true"
+      v-show="showSearch"
+      label-width="68px"
+    >
       <el-form-item label="店铺编号" prop="storeCode">
         <el-input
           v-model="queryParams.storeCode"
@@ -18,7 +25,12 @@
         />
       </el-form-item>
       <el-form-item label="店铺状态" prop="storeStatus">
-        <el-select v-model="queryParams.storeStatus" placeholder="请选择店铺状态" clearable>
+        <el-select
+          v-model="queryParams.storeStatus"
+          placeholder="请选择店铺状态"
+          clearable
+          class="query-select"
+        >
           <el-option
             v-for="dict in store_status"
             :key="dict.value"
@@ -35,21 +47,19 @@
           @keyup.enter="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="申请标记" prop="auditFlag">
-        <el-input
-          v-model="queryParams.auditFlag"
-          placeholder="请输入申请标记"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
+
+      <!-- 搜索框右侧按键 -->
       <el-form-item>
-        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button type="primary" icon="Search" @click="handleQuery"
+          >搜索</el-button
+        >
         <el-button icon="Refresh" @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
 
+    <!-- 功能按钮 -->
     <el-row :gutter="10" class="mb8">
+      <!-- 新增按钮 -->
       <el-col :span="1.5">
         <el-button
           type="primary"
@@ -57,8 +67,10 @@
           icon="Plus"
           @click="handleAdd"
           v-hasPermi="['manage:store:add']"
-        >新增</el-button>
+          >新增</el-button
+        >
       </el-col>
+      <!-- 修改按钮 -->
       <el-col :span="1.5">
         <el-button
           type="success"
@@ -67,8 +79,10 @@
           :disabled="single"
           @click="handleUpdate"
           v-hasPermi="['manage:store:edit']"
-        >修改</el-button>
+          >修改</el-button
+        >
       </el-col>
+      <!-- 删除按钮 -->
       <el-col :span="1.5">
         <el-button
           type="danger"
@@ -77,8 +91,10 @@
           :disabled="multiple"
           @click="handleDelete"
           v-hasPermi="['manage:store:remove']"
-        >删除</el-button>
+          >删除</el-button
+        >
       </el-col>
+      <!-- 导出按钮 -->
       <el-col :span="1.5">
         <el-button
           type="warning"
@@ -86,38 +102,83 @@
           icon="Download"
           @click="handleExport"
           v-hasPermi="['manage:store:export']"
-        >导出</el-button>
+          >导出</el-button
+        >
       </el-col>
-      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+      <!-- 搜索条显示控制工具栏 -->
+      <right-toolbar
+        v-model:showSearch="showSearch"
+        @queryTable="getList"
+      ></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="storeList" @selection-change="handleSelectionChange">
+    <!-- 数据表格 -->
+    <el-table
+      v-loading="loading"
+      :data="storeList"
+      @selection-change="handleSelectionChange"
+    >
+      <!-- 多选框 -->
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="店铺主键" align="center" prop="storeId" />
+      <!-- 序号-->
+      <el-table-column label="序号" type="index" align="center" width="55" />
+      <!-- 店铺编号 -->
       <el-table-column label="店铺编号" align="center" prop="storeCode" />
-      <el-table-column label="店铺logo" align="center" prop="storeLogo" width="100">
+      <!-- 店铺名称 -->
+      <el-table-column label="店铺名称" align="center" prop="storeName" />
+      <!-- 店铺logo -->
+      <el-table-column
+        label="店铺logo"
+        align="center"
+        prop="storeLogo"
+        width="100"
+      >
         <template #default="scope">
-          <image-preview :src="scope.row.storeLogo" :width="50" :height="50"/>
+          <image-preview :src="scope.row.storeLogo" :width="50" :height="50" />
         </template>
       </el-table-column>
-      <el-table-column label="店铺名称" align="center" prop="storeName" />
+
+      <!-- 店铺状态 -->
       <el-table-column label="店铺状态" align="center" prop="storeStatus">
         <template #default="scope">
-          <dict-tag :options="store_status" :value="scope.row.storeStatus"/>
+          <dict-tag :options="store_status" :value="scope.row.storeStatus" />
         </template>
       </el-table-column>
+      <!-- 联系电话 -->
       <el-table-column label="联系电话" align="center" prop="contact" />
+      <!-- 创建人 -->
       <el-table-column label="创建人" align="center" prop="createBy" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+
+      <!-- 功能按钮 -->
+      <el-table-column
+        label="操作"
+        align="center"
+        class-name="small-padding fixed-width"
+      >
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['manage:store:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['manage:store:remove']">删除</el-button>
+          <el-button
+            link
+            type="primary"
+            icon="Edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['manage:store:edit']"
+            >修改</el-button
+          >
+          <el-button
+            link
+            type="primary"
+            icon="Delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['manage:store:remove']"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
-    
+
+    <!-- 分页组件 -->
     <pagination
-      v-show="total>0"
+      v-show="total > 0"
       :total="total"
       v-model:page="queryParams.pageNum"
       v-model:limit="queryParams.pageSize"
@@ -126,15 +187,20 @@
 
     <!-- 添加或修改店铺管理对话框 -->
     <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+      <!-- 构造表单：rules校验每个form-item的prop -->
       <el-form ref="storeRef" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="店铺logo" prop="storeLogo">
-          <image-upload v-model="form.storeLogo"/>
+          <image-upload v-model="form.storeLogo" />
         </el-form-item>
         <el-form-item label="店铺名称" prop="storeName">
           <el-input v-model="form.storeName" placeholder="请输入店铺名称" />
         </el-form-item>
         <el-form-item label="店铺描述" prop="storeDesc">
-          <el-input v-model="form.storeDesc" type="textarea" placeholder="请输入内容" />
+          <el-input
+            v-model="form.storeDesc"
+            type="textarea"
+            placeholder="请输入内容"
+          />
         </el-form-item>
         <el-form-item label="联系电话" prop="contact">
           <el-input v-model="form.contact" placeholder="请输入联系电话" />
@@ -151,23 +217,48 @@
 </template>
 
 <script setup name="Store">
-import { listStore, getStore, delStore, addStore, updateStore } from "@/api/manage/store";
+// 导入API接口函数
+import {
+  listStore,
+  getStore,
+  delStore,
+  addStore,
+  updateStore,
+} from "@/api/manage/store";
 
+// 获取全局代理对象
 const { proxy } = getCurrentInstance();
-const { store_status } = proxy.useDict('store_status');
+// 获取字典数据：店铺状态
+const { store_status } = proxy.useDict("store_status");
 
+// 数据表格内容
 const storeList = ref([]);
-const open = ref(false);
-const loading = ref(true);
-const showSearch = ref(true);
-const ids = ref([]);
-const single = ref(true);
-const multiple = ref(true);
+
+// 存储总记录数
 const total = ref(0);
+// 对话框标题
 const title = ref("");
 
+// 对话框开关
+const open = ref(false);
+
+// 标记：是否显示主视图加载
+const loading = ref(true);
+// 标记：是否显示搜索栏
+const showSearch = ref(true);
+// 存储数据的ids（由多选框传递）
+const ids = ref([]);
+// 标记：是否仅选中一条数据
+const single = ref(true);
+// 标记：是否选中多条数据
+const multiple = ref(true);
+
+// 响应式数据对象
 const data = reactive({
+  // 表单数据
   form: {},
+  // 查询参数
+  // 若存在于搜索框，则与之关联，（好像）也就导致没法用于主视图数据以外的数据查询
   queryParams: {
     pageNum: 1,
     pageSize: 10,
@@ -175,25 +266,25 @@ const data = reactive({
     storeName: null,
     storeStatus: null,
     contact: null,
-    auditFlag: null,
-    userId: null,
   },
+  // 表单校验规则：增加/编辑店铺对话框
   rules: {
     storeLogo: [
-      { required: true, message: "店铺logo不能为空", trigger: "blur" }
+      { required: true, message: "店铺logo不能为空", trigger: "blur" },
     ],
     storeName: [
-      { required: true, message: "店铺名称不能为空", trigger: "blur" }
+      { required: true, message: "店铺名称不能为空", trigger: "blur" },
     ],
-  }
+  },
 });
 
+// 解构响应式数据
 const { queryParams, form, rules } = toRefs(data);
 
 /** 查询店铺管理列表 */
 function getList() {
   loading.value = true;
-  listStore(queryParams.value).then(response => {
+  listStore(queryParams.value).then((response) => {
     storeList.value = response.rows;
     total.value = response.total;
     loading.value = false;
@@ -222,7 +313,7 @@ function reset() {
     createTime: null,
     updateTime: null,
     createBy: null,
-    updateBy: null
+    updateBy: null,
   };
   proxy.resetForm("storeRef");
 }
@@ -241,7 +332,7 @@ function resetQuery() {
 
 // 多选框选中数据
 function handleSelectionChange(selection) {
-  ids.value = selection.map(item => item.storeId);
+  ids.value = selection.map((item) => item.storeId);
   single.value = selection.length != 1;
   multiple.value = !selection.length;
 }
@@ -256,8 +347,8 @@ function handleAdd() {
 /** 修改按钮操作 */
 function handleUpdate(row) {
   reset();
-  const _storeId = row.storeId || ids.value
-  getStore(_storeId).then(response => {
+  const _storeId = row.storeId || ids.value;
+  getStore(_storeId).then((response) => {
     form.value = response.data;
     open.value = true;
     title.value = "修改店铺管理";
@@ -266,16 +357,16 @@ function handleUpdate(row) {
 
 /** 提交按钮 */
 function submitForm() {
-  proxy.$refs["storeRef"].validate(valid => {
+  proxy.$refs["storeRef"].validate((valid) => {
     if (valid) {
       if (form.value.storeId != null) {
-        updateStore(form.value).then(response => {
+        updateStore(form.value).then((response) => {
           proxy.$modal.msgSuccess("修改成功");
           open.value = false;
           getList();
         });
       } else {
-        addStore(form.value).then(response => {
+        addStore(form.value).then((response) => {
           proxy.$modal.msgSuccess("新增成功");
           open.value = false;
           getList();
@@ -288,20 +379,35 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _storeIds = row.storeId || ids.value;
-  proxy.$modal.confirm('是否确认删除店铺管理编号为"' + _storeIds + '"的数据项？').then(function() {
-    return delStore(_storeIds);
-  }).then(() => {
-    getList();
-    proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {});
+  proxy.$modal
+    .confirm('是否确认删除店铺管理编号为"' + _storeIds + '"的数据项？')
+    .then(function () {
+      return delStore(_storeIds);
+    })
+    .then(() => {
+      getList();
+      proxy.$modal.msgSuccess("删除成功");
+    })
+    .catch(() => {});
 }
 
 /** 导出按钮操作 */
 function handleExport() {
-  proxy.download('manage/store/export', {
-    ...queryParams.value
-  }, `store_${new Date().getTime()}.xlsx`)
+  proxy.download(
+    "manage/store/export",
+    {
+      ...queryParams.value,
+    },
+    `store_${new Date().getTime()}.xlsx`
+  );
 }
 
+// 初始化列表数据
 getList();
 </script>
+
+<style scoped>
+.query-select {
+  width: 180px;
+}
+</style>
