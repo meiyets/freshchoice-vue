b6f1674c60dc2aaa26ea99d8df524da8f4937ed3
meiye_ts <hello@itcast.cn>
2025-05-08 16:24:04 +0800

    00-6缝缝补补：标签表、产品标签表生成代码初始化

---
+++ src/api/manage/product-tag.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询产品-标签关系列表
+export function listTag(query) {
+  return request({
+    url: "/manage/product-tag/list",
+    method: "get",
+    params: query,
+  });
+}
+
+// 查询产品-标签关系详细
+export function getTag(productTagId) {
+  return request({
+    url: "/manage/product-tag/" + productTagId,
+    method: "get",
+  });
+}
+
+// 新增产品-标签关系
+export function addTag(data) {
+  return request({
+    url: "/manage/product-tag",
+    method: "post",
+    data: data,
+  });
+}
+
+// 修改产品-标签关系
+export function updateTag(data) {
+  return request({
+    url: "/manage/product-tag",
+    method: "put",
+    data: data,
+  });
+}
+
+// 删除产品-标签关系
+export function delTag(productTagId) {
+  return request({
+    url: "/manage/product-tag/" + productTagId,
+    method: "delete",
+  });
+}
