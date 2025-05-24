fb07f866c1db502a50988ffc8102940ac3f07b5e
meiye_ts <hello@itcast.cn>
2025-05-24 23:31:46 +0800

    00-18缝缝补补：增加了许多菜单图标，并支持从自建目录导入

--- src/components/IconSelect/requireIcons.js
+++ src/components/IconSelect/requireIcons.js
@@ -1,8 +1,14 @@
-let icons = []
-const modules = import.meta.glob('./../../assets/icons/svg/*.svg');
+let icons = [];
+// 修改 import.meta.glob 以包含多个目录
+const modules = import.meta.glob([
+  "./../../assets/icons/svg/*.svg",
+  "./../../assets/icons/my_svg/*.svg", // 添加您的新目录
+]);
+
 for (const path in modules) {
-  const p = path.split('assets/icons/svg/')[1].split('.svg')[0];
+  // 调整路径处理逻辑，以正确提取不同目录下的图标名称
+  const p = path.split("/").pop().split(".svg")[0];
   icons.push(p);
 }
 
-export default icons
+export default icons;
